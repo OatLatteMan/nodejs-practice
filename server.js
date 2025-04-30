3923185ea78cbedf50acd9880ccbf2b6e74293c0
Dmitrii Panov <15minstan@gmail.com>
2025-05-01 00:37:00 +0200

    nodejs practice

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, Node.js world!');
+  const parsedUrl = url.parse(req.url, true);
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  if (parsedUrl.pathname === '/') {
+    res.end('<h1>Welcome to the Home Page</h1>');
+  } else if (parsedUrl.pathname === '/about') {
+    res.end('<h1>About Us</h1><p>This is the about page.</p>');
+  } else if (parsedUrl.pathname === '/contact') {
+    res.end('<h1>Contact</h1><p>Email: example@example.com</p>');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>404 Not Found</h1>');
+  }
 });
 
-const PORT = 3000;
+const PORT = 2089;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
