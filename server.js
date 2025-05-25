6ddf135debe24a5467bdd7ad5e839f8517ac3990
Dmitrii Panov <15minstan@gmail.com>
2025-05-25 23:53:35 +0200

    nodejs practice

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ app.use(session({
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/protected-routes', protectedRoutes);
 
 // Products routes
